5630ded297df079048d6805d4d9b854390ec6cdd
Nitin Jain <j9.nitin@gmail.com>
2020-05-01 19:55:35 +0530

    Awares Added

--- src/main/java/org/spring/beans/Triangle1.java
+++ src/main/java/org/spring/beans/Triangle1.java
@@ -1,10 +1,18 @@
 package org.spring.beans;
 
-public class Triangle1 {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle1 implements ApplicationContextAware, BeanNameAware{
 
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
+	private ApplicationContext context;
+	private String beanName;
+	
 	public Point getPointA() {
 		return pointA;
 	}
@@ -29,5 +37,23 @@ public class Triangle1 {
 		System.out.println("PointB: "+pointB);
 		System.out.println("PointC: "+pointC);
 	}
+	
+	public void useAppContext(){
+		Triangle1 triangle = (Triangle1) context.getBean("triangle1");
+		System.out.println("----------------VIA APP CONTEXT AWARE--------------");
+		triangle.show();
+		System.out.println("----------------VIA BEAN NAME AWARE--------------");
+		System.out.println("Bean name is: "+beanName);
+	}
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context = applicationContext;
+	}
+	
+	@Override
+	public void setBeanName(String name) {
+		beanName = name;
+	}
+
 	
 }
