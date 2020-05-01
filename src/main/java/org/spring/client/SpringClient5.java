55dfb10bbb7cc873263f5269276e5caf3a1157b1
Nitin Jain <j9.nitin@gmail.com>
2020-05-01 19:21:54 +0530

    Extend1: Injecting Objects Added

---
+++ src/main/java/org/spring/client/SpringClient5.java
@@ -0,0 +1,16 @@
+package org.spring.client;
+
+import org.spring.beans.Employee;
+import org.spring.beans.Triangle1;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class SpringClient5 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/spring4.xml");
+		Triangle1 triangle1 = (Triangle1) context.getBean("triangle1");
+		triangle1.show();
+	}
+}
