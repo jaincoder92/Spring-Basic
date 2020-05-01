a832fba2a0f49005286cb5284325032605b091e6
Nitin Jain <j9.nitin@gmail.com>
2020-05-01 17:52:23 +0530

    Second Commit

---
+++ src/main/java/org/spring/beans/Triangle.java
@@ -0,0 +1,35 @@
+package org.spring.beans;
+
+public class Triangle {
+
+	private int side1;
+	private int side2;
+	private int side3;
+	
+	
+	public int getSide1() {
+		return side1;
+	}
+	public void setSide1(int side1) {
+		this.side1 = side1;
+	}
+	public int getSide2() {
+		return side2;
+	}
+	public void setSide2(int side2) {
+		this.side2 = side2;
+	}
+	public int getSide3() {
+		return side3;
+	}
+	public void setSide3(int side3) {
+		this.side3 = side3;
+	}
+	public void draw() {
+		System.out.println("Side1 is: "+side1);
+		System.out.println("Side2 is: "+side2);
+		System.out.println("Side3 is: "+side3);
+		
+	}
+	
+}
---
+++ src/main/java/org/spring/client/SpringClient1.java
@@ -0,0 +1,20 @@
+package org.spring.client;
+
+import java.io.File;
+
+import org.spring.beans.Triangle;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+public class SpringClient1 {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource(("src/main/resources/spring1.xml")));
+		Triangle triangle = (Triangle) factory.getBean("triangle");
+		triangle.draw();
+		
+	}
+}
