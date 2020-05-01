db13e729e50ee9b8669b756965165c7b1110213a
Nitin Jain <j9.nitin@gmail.com>
2020-05-01 19:24:06 +0530

    Alias Added

---
+++ src/main/java/org/spring/client/SpringClient6.java
@@ -0,0 +1,16 @@
+package org.spring.client;
+
+import org.spring.beans.Employee;
+import org.spring.beans.Triangle1;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class SpringClient6 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/spring5.xml");
+		Triangle1 triangle1 = (Triangle1) context.getBean("triangle1-alias-1");
+		triangle1.show();
+	}
+}
